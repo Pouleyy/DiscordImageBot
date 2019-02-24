664c3cc318ffbd5644b3914175033de7c4bf90c6
Kevin Harismendy <kevin.harismendy@outlook.com>
2019-02-24 20:10:22 +0100

    :art: Use async await for subreddit.js

--- src/commands/subreddit.js
+++ src/commands/subreddit.js
@@ -1,144 +1,220 @@
 import logger from "../server/logger";
 import subCtrl from "../controllers/subreddit";
-import request from "request";
 import { RichEmbed } from "discord.js";
 import utils from "../utils/utils";
+import axios from "axios";
 
 function get(args, message) {
-    const sub = args.shift();
-    return subCtrl.getSubreddit(sub)
-        .then(subSearched => {
-            if (!subSearched) {
-                sub ? utils.sendErrorEmbed(message, `No matching subreddit ${sub}, sorry ${utils.sadEmojiPicker()}\nDon't forget to use the search command if you're not sure`) : utils.sendErrorEmbed(message, `You need to provide a subreddit so I can get gifs and pics for you ${utils.sadEmojiPicker()}`);
-            } else {
-                let images = [];
-                if (message.channel.nsfw) {
-                    args.includes("bomb") ? getMedia(subSearched, args, message, images, 5) : getMedia(subSearched, args, message, images, 1);
-                } else {
-                    subSearched.nsfw ? utils.sendErrorEmbed(message, `Sorry it seems that you're in a SFW channel and you request a NSFW subreddit ${utils.sadEmojiPicker()}`) : args.includes("bomb") ? getMedia(subSearched, args, message, images, 5) : getMedia(subSearched, args, message, images, 1);
-                }
-            }
-        })
-        .catch(err => logger.error(err));
-}
-
-function getMedia(sub, args, message, images, length) {
-    makeRequest(sub.name, function (imageURL) {
-        if (imageURL == null) {
-            utils.sendErrorEmbed(message, `Error while requesting ${sub}, sorry ${utils.sadEmojiPicker()}`);
+  const sub = args.shift();
+  return subCtrl
+    .getSubreddit(sub)
+    .then(subSearched => {
+      if (!subSearched) {
+        sub
+          ? utils.sendErrorEmbed(
+              message,
+              `No matching subreddit ${sub}, sorry ${utils.sadEmojiPicker()}\nDon't forget to use the search command if you're not sure`
+            )
+          : utils.sendErrorEmbed(
+              message,
+              `You need to provide a subreddit so I can get gifs and pics for you ${utils.sadEmojiPicker()}`
+            );
+      } else {
+        let images = [];
+        if (message.channel.nsfw) {
+          args.includes("bomb")
+            ? getMedia(subSearched, args, message, images, 5)
+            : getMedia(subSearched, args, message, images, 1);
         } else {
-            if (args.includes("gif") && sub.canIGifIt) {
-                images = images.concat(imageURL.filter(url => url.includes(".mp4") || url.includes(".webm")));
-            } else if (args.includes("pic")) {
-                images = images.concat(imageURL.find(url => url.includes(".jpg")));
-            } else {
-                images = images.concat(imageURL);
-            }
-            if (images.length >= length) {
-                utils.loggerDiscord(message, `Request ${length} subreddit ${sub.name}`);
-                const randomColor = utils.randomColor();
-                images.slice(0, length).map(image => {
-                    if (image.includes(".jpg")) {
-                        const embed = new RichEmbed()
-                            .setColor(randomColor)
-                            .setImage(image);
-                        if (length === 1) embed.setDescription(`You can use the **bomb** option to get more content\nTry *!s ${sub.name} bomb*`);
-                        message.channel.send({ embed });
-                    } else {
-                        if (length === 1) image = `You can use the **bomb** option to get more content\nTry *!s ${sub.name} bomb*\n${image}`
-                        message.channel.send(image);
-                    }
-                })
-            } else {
-                getMedia(sub, args, message, images, length);
-            }
+          subSearched.nsfw
+            ? utils.sendErrorEmbed(
+                message,
+                `Sorry it seems that you're in a SFW channel and you request a NSFW subreddit ${utils.sadEmojiPicker()}`
+              )
+            : args.includes("bomb")
+            ? getMedia(subSearched, args, message, images, 5)
+            : getMedia(subSearched, args, message, images, 1);
         }
-    });
+      }
+    })
+    .catch(err => logger.error(err));
 }
 
-function makeRequest(sub, callback) {
-    const HEADER = {
-        "Content-Type": "application/json"
-    };
-    const TARGET_URL = `https://scrolller.com/api/random/${sub}`;
-    const METHOD = "GET";
-    request({
-        headers: HEADER,
-        url: TARGET_URL,
-        method: METHOD,
-    }, function (err, res, body) {
-        if (err || res.statusCode != 200 || body.length < 1) {
-            logger.error("Error while scraping scrolller.com ", JSON.stringify(body));
-            callback(null);
-        } else {
-            callback(JSON.parse(body));
-        }
-    });
-}
+const getMedia = async (sub, args, message, images, length) => {
+  try {
+    let imageURL = await makeRequest(sub.name);
+    if (imageURL == null) {
+      utils.sendErrorEmbed(
+        message,
+        `Error while requesting ${sub}, sorry ${utils.sadEmojiPicker()}`
+      );
+    } else {
+      if (args.includes("gif") && sub.canIGifIt) {
+        images = images.concat(
+          imageURL.filter(url => url.includes(".mp4") || url.includes(".webm"))
+        );
+      } else if (args.includes("pic")) {
+        images = images.concat(imageURL.find(url => url.includes(".jpg")));
+      } else {
+        images = images.concat(imageURL);
+      }
+      images.length >= length
+        ? sendContent(images, length, message, sub)
+        : getMedia(sub, args, message, images, length, sub);
+    }
+  } catch (err) {
+    utils.sendErrorEmbed(
+      message,
+      `Error while requesting ${sub}, sorry ${utils.sadEmojiPicker()}`
+    );
+    logger.error(`Error while getting media in ${sub.name}`);
+  }
+};
 
+const sendContent = async (images, length, message, sub) => {
+  utils.loggerDiscord(message, `Request ${length} subreddit ${sub.name}`);
+  const randomColor = utils.randomColor();
+  images.slice(0, length).map(image => {
+    if (image.includes(".jpg")) {
+      const embed = new RichEmbed().setColor(randomColor).setImage(image);
+      if (length === 1)
+        embed.setDescription(
+          `You can use the **bomb** option to get more content\nTry *!s ${
+            sub.name
+          } bomb*`
+        );
+      message.channel.send({ embed });
+    } else {
+      if (length === 1)
+        image = `You can use the **bomb** option to get more content\nTry *!s ${
+          sub.name
+        } bomb*\n${image}`;
+      message.channel.send(image);
+    }
+  });
+};
 
-function search(args, message) {
-    const sub = args[0];
-    subCtrl.searchSubreddit(sub)
-        .then(subs => {
-            if (subs.length === 0) {
-                utils.sendErrorEmbed(message, `No matching subreddit to your search "${sub}", sorry ${utils.sadEmojiPicker()}`);
-            } else {
-                utils.loggerDiscord(message, `Searched subreddit ${sub} ${subs.length} match found`);
-                const subsWithOnlyName = utils.extractName(subs, "!s ");
-                const arrays = utils.divideInMultipleArrays(subsWithOnlyName, 30);
-                const embeds = utils.divideInMultipleEmbed(arrays, 18);
-                embeds.map((embed, index) => {
-                    embed.setTitle(`Search ${index + 1}/${embeds.length} for subreddit : ${sub}`);
-                    embed.setColor(utils.randomColor());
-                    message.channel.send({ embed });
-                });
-            }
-        })
-        .catch(err => logger.error(err));
-}
+const makeRequest = async sub => {
+  const TARGET_URL = `https://scrolller.com/api/random/${sub}`;
+  try {
+    const response = await axios.get(TARGET_URL);
+    const data = response.data;
+    return data;
+  } catch (err) {
+    logger.error("Error while scraping scrolller.com ", err);
+    return null;
+  }
+};
 
-function info(args, message) {
-    const sub = args[0];
-    utils.loggerDiscord(message, `Info on subreddit ${sub}`);
-    subCtrl.getSubreddit(sub)
-        .then(subFound => {
-            if (!subFound) {
-                utils.sendErrorEmbed(message, `No matching subreddit for info on "${sub}", sorry ${utils.sadEmojiPicker()}`);
-                return;
-            } else {
-                makeRequest(subFound.name, function (imageURL) {
-                    const thumbnail = !imageURL ? null : message.channel.nsfw ? imageURL.find(url => url.includes(".jpg")) : subFound.nsfw ? null : imageURL.find(url => url.includes(".jpg"));
-                    const embed = new RichEmbed();
-                    embed.setColor(utils.randomColor());
-                    embed.setTitle(sub[0].toUpperCase() + sub.slice(1));
-                    embed.setURL(`https://www.reddit.com/r/${sub}`);
-                    embed.setDescription(subFound.title);
-                    embed.setThumbnail(thumbnail);
+const search = async (args, message) => {
+  const sub = args[0];
+  try {
+    const subs = await subCtrl.searchSubreddit(sub);
+    if (subs.length === 0) {
+      utils.sendErrorEmbed(
+        message,
+        `No matching subreddit to your search "${sub}", sorry ${utils.sadEmojiPicker()}`
+      );
+    } else {
+      utils.loggerDiscord(
+        message,
+        `Searched subreddit ${sub} ${subs.length} match found`
+      );
+      const subsWithOnlyName = utils.extractName(subs, "!s ");
+      const arrays = utils.divideInMultipleArrays(subsWithOnlyName, 30);
+      const embeds = utils.divideInMultipleEmbed(arrays, 18);
+      embeds.map((embed, index) => {
+        embed.setTitle(
+          `Search ${index + 1}/${embeds.length} for subreddit : ${sub}`
+        );
+        embed.setColor(utils.randomColor());
+        message.channel.send({ embed });
+      });
+    }
+  } catch (error) {
+    logger.error(`Error while searching ${sub}`, error);
+  }
+};
 
-                    subFound.description ? embed.addField("Description", subFound.description) : "embed.addBlankField() //Not so pretty :/";
-                    embed.addField("Age", subFound.age, true);
-                    embed.addField("Subscriber", subFound.subscriber, true);
-                    thumbnail ? "" : embed.addField("\u200B", "\u200B", true);
+const info = async (args, message) => {
+  const sub = args[0];
+  utils.loggerDiscord(message, `Info on subreddit ${sub}`);
+  try {
+    const subFound = await subCtrl.getSubreddit(sub);
+    if (!subFound) {
+      utils.sendErrorEmbed(
+        message,
+        `No matching subreddit for info on "${sub}", sorry ${utils.sadEmojiPicker()}`
+      );
+    } else {
+      try {
+        const url = await makeRequest(subFound.name);
+        const thumbnail = !url
+          ? null
+          : message.channel.nsfw
+          ? url.find(url => url.includes(".jpg"))
+          : subFound.nsfw
+          ? null
+          : url.find(url => url.includes(".jpg"));
+        const embed = new RichEmbed();
+        embed.setColor(utils.randomColor());
+        embed.setTitle(sub[0].toUpperCase() + sub.slice(1));
+        embed.setURL(`https://www.reddit.com/r/${sub}`);
+        embed.setDescription(subFound.title);
+        embed.setThumbnail(thumbnail);
 
-                    embed.addField("Similar subreddit", subFound.similarSub.length == 0 ? "None 🤷" : utils.arrayToString(subFound.similarSub));
+        subFound.description
+          ? embed.addField("Description", subFound.description)
+          : "embed.addBlankField() //Not so pretty :/";
+        embed.addField("Age", subFound.age, true);
+        embed.addField("Subscriber", subFound.subscriber, true);
+        thumbnail ? "" : embed.addField("\u200B", "\u200B", true);
 
-                    embed.addField("Pictures", subFound.nbPics, true);
-                    embed.addField("Gifs", subFound.nbGifs, true);
-                    embed.addField("NSFW", subFound.nsfw ? "Totally NSFW" : "Nah, SFW", true);
+        embed.addField(
+          "Similar subreddit",
+          subFound.similarSub.length == 0
+            ? "None 🤷"
+            : utils.arrayToString(subFound.similarSub)
+        );
 
-                    embed.addField("Category", subFound.categoryName.length == 0 ? "Not in a category sorry 😕" : utils.arrayToString(subFound.categoryName), true);
-                    embed.addField("Related category", subFound.relatedCategory.length == 0 ? "No related category sorry 😞" : utils.arrayToString(subFound.relatedCategory), true);
+        embed.addField("Pictures", subFound.nbPics, true);
+        embed.addField("Gifs", subFound.nbGifs, true);
+        embed.addField(
+          "NSFW",
+          subFound.nsfw ? "Totally NSFW" : "Nah, SFW",
+          true
+        );
 
-                    message.channel.send({ embed });
-                });
-            }
-        })
-        .catch(err => logger.error(err));
-}
+        embed.addField(
+          "Category",
+          subFound.categoryName.length == 0
+            ? "Not in a category sorry 😕"
+            : utils.arrayToString(subFound.categoryName),
+          true
+        );
+        embed.addField(
+          "Related category",
+          subFound.relatedCategory.length == 0
+            ? "No related category sorry 😞"
+            : utils.arrayToString(subFound.relatedCategory),
+          true
+        );
+
+        message.channel.send({ embed });
+      } catch (error) {
+        logger.error(
+          `Problem while requesting thumbnail for sub ${sub}`,
+          error
+        );
+      }
+    }
+  } catch (error) {
+    logger.error(`Problem while searching info on ${sub}`, error);
+  }
+};
 
 export default {
-    get,
-    search,
-    info,
+  get,
+  search,
+  info
 };
