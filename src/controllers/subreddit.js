b92f1db7ee435ec80ce224baa08f6c065e3b2239
Kevin Harismendy <kevin.harismendy@outlook.com>
2019-03-29 15:00:20 +0100

    :art: Update function declaration

--- src/controllers/subreddit.js
+++ src/controllers/subreddit.js
@@ -5,22 +5,20 @@ import Subreddit from "../models/subreddit";
  * @returns {Promise}
  * @param {String} subreddit - Subreddit
  */
-function getSubreddit(subreddit) {
-    return Subreddit.get(subreddit);
-}
+const getSubreddit = subreddit => {
+  return Subreddit.get(subreddit);
+};
 
 /**
  *
  * @returns {Promise}
  * @param {String} subreddit - Subreddit
  */
-function searchSubreddit(subreddit) {
-    return Subreddit.search(subreddit);
-}
-
-
+const searchSubreddit = subreddit => {
+  return Subreddit.search(subreddit);
+};
 
 export default {
-    getSubreddit,
-    searchSubreddit,
+  getSubreddit,
+  searchSubreddit
 };
