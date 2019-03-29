b9cf844f789e020640ebf85554a160857de34b1e
Kevin Harismendy <kevin.harismendy@outlook.com>
2019-03-29 14:56:22 +0100

    :card_file_box: Add a remove method to subreddit and category

--- src/models/category.js
+++ src/models/category.js
@@ -1,107 +1,119 @@
 import mongoose from "../server/mongo";
 
-export const CategorySchema = new mongoose.Schema({
+export const CategorySchema = new mongoose.Schema(
+  {
     id: {
-        type: Number,
-        unique: true,
+      type: Number,
+      unique: true
     },
     name: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true
     },
     relatedCategory: {
-        type: [String]
+      type: [String]
     },
-    subreddits: [{
+    subreddits: [
+      {
         name: {
-            type: String
+          type: String
         },
         id: {
-            type: Number
+          type: Number
         }
-    }],
+      }
+    ],
     nbPics: {
-        type: Number
+      type: Number
     },
     nbGifs: {
-        type: String
+      type: String
     },
     nsfw: {
-        type: Boolean
+      type: Boolean
     }
-}, {
-        timestamps: {}
-    });
+  },
+  {
+    timestamps: {}
+  }
+);
 
 /**
  * Statics
  */
 
 CategorySchema.statics = {
-    /**
-    * Save Category
-    * @param {Number} id - id of the category
-    * @param {String} category - Category
-    * @returns {Promise}
-    */
-    createOrUpdate(category) {
-        return this.get(category.name)
-            .then(exist => {
-                let cat;
-                if (exist) {
-                    cat = exist;
-                }
-                else {
-                    cat = new this();
-                }
-                cat.id = category.id;
-                cat.name = category.name;
-                cat.relatedCategory = category.relatedCategory;
-                cat.subreddits = category.subreddits;
-                cat.nbPics = category.nbPics;
-                cat.nbGifs = category.nbGifs;
-                cat.nsfw = category.nsfw;
-                return cat.save();
-            });
-    },
+  /**
+   * Save Category
+   * @param {Number} id - id of the category
+   * @param {String} category - Category
+   * @returns {Promise}
+   */
+  createOrUpdate(category) {
+    return this.get(category.name).then(exist => {
+      let cat;
+      if (exist) {
+        cat = exist;
+      } else {
+        cat = new this();
+      }
+      cat.id = category.id;
+      cat.name = category.name;
+      cat.relatedCategory = category.relatedCategory;
+      cat.subreddits = category.subreddits;
+      cat.nbPics = category.nbPics;
+      cat.nbGifs = category.nbGifs;
+      cat.nsfw = category.nsfw;
+      return cat.save();
+    });
+  },
 
-    /**
-     * Get Category
-     * @param {String} category
-     * @returns {Promise}
-     */
-    get(category) {
-        return this.findOne({
-            name: category
-        });
-    },
+  /**
+   * Get Category
+   * @param {String} category
+   * @returns {Promise}
+   */
+  get(category) {
+    return this.findOne({
+      name: category
+    });
+  },
 
-    /**
-     * Remove all Category
-     * @returns {Promise}
-     */
-    delete() {
-        return this.remove({});
-    },
-    /**
-     * Get all Category
-     * @returns {Promise}
-     */
-    all() {
-        return this.find({});
-    },
+  /**
+   * Remove one Category
+   * @param {String} category Name of the category to delete
+   * @returns {Promise}
+   */
+  remove(category) {
+    return this.deleteOne({ name: category });
+  },
 
-    /**
-     * Search Category
-     * @param {String} category
-     * @returns {Promise}
-     */
-    search(category) {
-        return this.find({
-            name: { $regex: category, $options: "i" }
-        }).select({ "name": 1, "_id": 0 });
-    }
+  /**
+   * Remove all Category
+   * @returns {Promise}
+   */
+  delete() {
+    return this.remove({});
+  },
+  /**
+   * Get all Category
+   * @returns {Promise}
+   */
+  all() {
+    return this.find({});
+  },
+
+  /**
+   * Search Category
+   * @param {String} category
+   * @returns {Promise}
+   */
+  search(category) {
+    return this.find({
+      name: { $regex: category, $options: "i" }
+    }).select({ name: 1, _id: 0 });
+  }
 };
 
 /**
--- src/models/subreddit.js
+++ src/models/subreddit.js
@@ -1,123 +1,132 @@
 import mongoose from "../server/mongo";
 
-export const SubredditSchema = new mongoose.Schema({
+export const SubredditSchema = new mongoose.Schema(
+  {
     id: {
-        type: Number,
-        require: true,
-        unique: true
+      type: Number,
+      require: true,
+      unique: true
     },
     name: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true
     },
     canIGifIt: {
-        type: Boolean,
-        require: true,
+      type: Boolean,
+      require: true
     },
     categoryName: {
-        type: [String],
+      type: [String]
     },
     relatedCategory: {
-        type: [String]
+      type: [String]
     },
     title: {
-        type: String,
+      type: String
     },
     subscriber: {
-        type: Number,
+      type: Number
     },
     description: {
-        type: String,
+      type: String
     },
     age: {
-        type: String,
+      type: String
     },
     similarSub: {
-        type: [String],
+      type: [String]
     },
     nbPics: {
-        type: Number,
+      type: Number
     },
     nbGifs: {
-        type: Number,
+      type: Number
     },
     nsfw: {
-        type: Boolean,
+      type: Boolean
     }
-}, {
-        timestamps: {}
-    });
+  },
+  {
+    timestamps: {}
+  }
+);
 
 SubredditSchema.statics = {
-    /**
-    * Create Subreddit or update
-    * @param {Subreddit} subToSave
-    * @returns {Promise}
-    */
-    createOrUpdate(subToSave) {
-        return this.get(subToSave.name)
-            .then(exist => {
-                let sub;
-                if (exist) {
-                    sub = exist;
-                }
-                else {
-                    sub = new this();
-                }
-                sub.id = subToSave.id;
-                sub.name = subToSave.name;
-                sub.canIGifIt = subToSave.canIGifIt;
-                sub.categoryName = subToSave.categoryName;
-                sub.relatedCategory = subToSave.relatedCategory;
-                sub.title = subToSave.title;
-                sub.subscriber = subToSave.subscriber;
-                sub.description = subToSave.description;
-                sub.age = subToSave.age;
-                sub.similarSub = subToSave.similarSub;
-                sub.nbPics = subToSave.nbPics;
-                sub.nbGifs = subToSave.nbGifs;
-                sub.nsfw = subToSave.nsfw;
-                return sub.save();
-            });
-    },
+  /**
+   * Create Subreddit or update
+   * @param {Subreddit} subToSave
+   * @returns {Promise}
+   */
+  createOrUpdate(subToSave) {
+    return this.get(subToSave.name).then(exist => {
+      let sub;
+      if (exist) {
+        sub = exist;
+      } else {
+        sub = new this();
+      }
+      sub.id = subToSave.id;
+      sub.name = subToSave.name;
+      sub.canIGifIt = subToSave.canIGifIt;
+      sub.categoryName = subToSave.categoryName;
+      sub.relatedCategory = subToSave.relatedCategory;
+      sub.title = subToSave.title;
+      sub.subscriber = subToSave.subscriber;
+      sub.description = subToSave.description;
+      sub.age = subToSave.age;
+      sub.similarSub = subToSave.similarSub;
+      sub.nbPics = subToSave.nbPics;
+      sub.nbGifs = subToSave.nbGifs;
+      sub.nsfw = subToSave.nsfw;
+      return sub.save();
+    });
+  },
 
-    /**
-     * Get Subreddit
-     * @param {String} subName
-     * @returns {Promise}
-     */
-    get(subName) {
-        return this.findOne({
-            name: subName
-        });
-    },
+  /**
+   * Get Subreddit
+   * @param {String} subName
+   * @returns {Promise}
+   */
+  get(subName) {
+    return this.findOne({
+      name: subName
+    });
+  },
 
-    /**
-     * Remove all Subreddit
-     * @returns {Promise}
-     */
-    delete() {
-        return this.remove({});
-    },
-    /**
-     * Get all Subreddit
-     * @returns {Promise}
-     */
-    all() {
-        return this.find({});
-    },
+  /**
+   * Remove one Subreddit
+   * @param {String} sub Name of the subreddit to delete
+   * @returns {Promise}
+   */
+  remove(sub) {
+    return this.deleteOne({ name: sub });
+  },
+  /**
+   * Remove all Subreddit
+   * @returns {Promise}
+   */
+  delete() {
+    return this.remove({});
+  },
+  /**
+   * Get all Subreddit
+   * @returns {Promise}
+   */
+  all() {
+    return this.find({});
+  },
 
-    /**
-     * Search Subreddit
-     * @param {String} subName
-     * @returns {Promise}
-     */
-    search(subName) {
-        return this.find({
-            name: { $regex: subName, $options: "i" }
-        }).select({ "name": 1, "_id": 0 });
-    }
+  /**
+   * Search Subreddit
+   * @param {String} subName
+   * @returns {Promise}
+   */
+  search(subName) {
+    return this.find({
+      name: { $regex: subName, $options: "i" }
+    }).select({ name: 1, _id: 0 });
+  }
 };
 
 /**
